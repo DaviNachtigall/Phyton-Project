7e035cd2d2e2887a039dd7608a47b68ad8625d5e
Davi <nachtigalldavi@gmail.com>
2026-08-28 01:21:44 -0300

    Composição feita, mas com bugs

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def desenhar_grade():
 def desenhar_elementos():
     for i in range(Grade):
         for j in range(Grade):
-            CentroX = EixoX + j * TamSquare + TamSquare //   2
+            CentroX = EixoX + j * TamSquare + TamSquare //  2
             CentroY = EixoY + i * TamSquare + TamSquare // 2
 
             valor = Campo[i][j]
@@ -70,3 +70,190 @@ def desenhar_elementos():
                 pygame.draw.circle(Tela, CorG1, (CentroX, CentroY), TamSquare // 3)
                 texto = FontePequena.render("G1", True, (0, 0, 0))
                 Tela.blit(texto, (CentroX, CentroY))
+
+            #G2
+            elif valor == -2:
+                pygame.draw.circle(Tela, CorG2, (CentroX, CentroY), TamSquare // 3)
+                texto = FontePequena.render("G2", True, (0, 0, 0))
+                Tela.blit(texto, (CentroX, CentroY))
+
+            #Lixo
+            elif valor > 0:
+                TamanhoLixo = int((TamSquare // 2) * (valor / 20) + 5)
+                RectLixo = pygame.Rect(0, 0, TamanhoLixo, TamanhoLixo)
+                RectLixo.center = (CentroX, CentroY)
+                pygame.draw.rect(Tela, CorLixo, RectLixo)
+
+                TextoTempo = FontePequena.render(str(valor), True, CorText)
+                Tela.blit(TextoTempo, TextoTempo.get_rect(center=(CentroX, CentroY + TamanhoLixo // 2 + 5)))
+
+def desenhar_interface():
+    TextoTitulo = FonteGrande.render("CORRIDA GUAXINIM", True, CorText)
+    Tela.blit(TextoTitulo, TextoTitulo.get_rect(center=(LarguraPixel // 2, 40)))
+
+    if Termino == 0:
+        CorTurno = CorG1 if Turno == 1 else CorG2
+        TextoTurno = FontePequena.render(f"Turno: Jogador {Turno}", True, CorTurno)
+        Tela.blit(TextoTurno, TextoTurno.get_rect(center=(LarguraPixel // 2, AlturaPixel - 40)))
+
+    else:
+        if Termino == 1:
+            CorTermino = CorG1
+            TextoVitoria = FonteGrande.render("Guaxinim 1 venceu!!", True, CorTermino)
+            Tela.blit(TextoVitoria, TextoVitoria.get_rect(center=(LarguraPixel // 2, AlturaPixel - 40)))
+        else:
+            CorTermino = CorG2
+            TextoVitoria = FonteGrande.render("Guaxinim 2 venceu!!", True, CorTermino)
+            Tela.blit(TextoVitoria, TextoVitoria.get_rect(center=(LarguraPixel // 2, AlturaPixel - 40)))
+
+    # Instruções laterais
+
+    Instr1 = [
+
+        "G1 (Verde)",
+
+        "W: Cima", "S: Baixo",
+
+        "A: Esq", "D: Dir",
+
+        "Objetivo:", "Chegar na Dir."
+
+    ]
+
+    Instr2 = [
+
+        "G2 (Azul)",
+
+        "I: Cima", "K: Baixo",
+
+        "J: Esq", "L: Dir",
+
+        "Objetivo:", "Chegar na Esq."
+
+    ]
+
+    # Instruções Laterais
+    # Percorre as listas de texto usando enumerate() para obter a frase e o índice da linha (indice = 0, 1, 2...).
+    # A fórmula '100 + idx * 25' calcula a posição vertical (Y) de cada frase, pulando 25 pixels por linha
+    # para que os textos fiquem organizados verticalmente e não sejam desenhados uns sobre os outros.
+    # G1 é desenhado na margem esquerda (X = 20) e G2 na margem direita (X = LARGURA_TELA - 150).
+
+    for indice, linha in enumerate(Instr1):
+
+        Txt = FontePequena.render(linha, True, CorG1)
+
+        Tela.blit(Txt, (20, 100 + indice * 25))
+
+    for indice, linha in enumerate(Instr2):
+
+        Txt = FontePequena.render(linha, True, CorG1)
+
+        Tela.blit(Txt, (LarguraPixel - 150, 100 + indice * 25))
+
+def jogada(dx,dy):
+    global PosG1, PosG2, Turno
+
+    GuaxininNum = -1 if Turno == 1 else -2
+
+    PosAtual = PosG1 if Turno == 1 else PosG2
+
+    PosNovaX = PosAtual[0] + dx
+    PosNovaY = PosAtual[1] + dy
+
+    if PosNovaX > 7 or PosNovaX < 0 or PosNovaY > 7 or PosNovaY < 0 or Campo[PosNovaX][PosNovaY] != 0:
+        print("Movimento inválido!")
+        return False
+
+    else:
+        Campo[PosAtual[0]][PosAtual[1]] = 20  # Deixa lixo
+        # Atualiza posicao
+        Campo[PosNovaX][PosNovaY] = GuaxininNum
+
+        if(GuaxininNum == -1):
+            PosG1 = [PosNovaX, PosNovaY]
+        else:
+            PosG2 = [PosNovaX, PosNovaY]
+
+        return True
+
+def renovar_lixo():
+
+    for i in range(Grade):
+        for j in range(Grade):
+            if Campo[i][j] > 0:
+                Campo[i][j] -= 1
+    return
+
+
+def checar_vitoria():
+    if PosG1[0] == 7:
+        Termino = 1
+    elif PosG2[1] == 0:
+        Termino = 2
+    else:
+        return
+
+def main():
+    global Turno, Termino
+    while Termino == 0:
+        MovimentoFeito = False
+        #verifica se algum evento/condicao faz o jogo terminar
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        if event.type == pygame.KEYDOWN and Termino == 0:
+            dx, dy = 0, 0
+
+            #AWSD Tipo de jogada de G1
+            if Turno == 1:
+                if event.key == pygame.K_w:
+                    dy = -1
+                elif event.key == pygame.K_s:
+                    dy = 1
+                elif event.key == pygame.K_a:
+                    dx = -1
+                elif event.key == pygame.K_d:
+                    dx = 1
+
+            #JKIL tipo de jogada de G2
+            elif Turno == 2:
+                if event.key == pygame.K_i:
+                    dy = -1
+                elif event.key == pygame.K_k:
+                    dy = 1
+                elif event.key == pygame.K_j:
+                    dx = -1
+                elif event.key == pygame.K_l:
+                    dx = 1
+
+            #atualizar variavel MovimentoFeito
+            if dx != 0 or dy != 0:
+                if jogada(dx, dy):
+                    MovimentoFeito = True
+
+            if MovimentoFeito:
+                renovar_lixo()
+                checar_vitoria()
+
+
+            if Termino == 0 and Turno == 1: Turno = 2
+            else: Turno = 1
+
+            # 3. Desenho (Renderização)
+            Tela.fill(CorFundo)
+            desenhar_grade()
+            desenhar_elementos()
+            desenhar_interface()
+
+            # Atualiza a tela gráfica
+            pygame.display.flip()
+
+            # Mantém o jogo rodando a 60 FPS (quadros por segundo)
+            Relogio.tick(60)
+
+    if __name__ == "__main__":
+        main()
+
+
